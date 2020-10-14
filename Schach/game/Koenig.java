5fa4d21e401b686659bd5231d77a7838b1ebfda3
myhpn <marcel.maffey@gmail.com>
2020-10-14 15:54:06 +0200

    König drin

---
+++ Schach/game/Koenig.java
@@ -0,0 +1,30 @@
+package game;
+
+public class Koenig extends Figur {
+	
+	public Koenig(boolean farbeWeiss, boolean bewegt) {
+		super(farbeWeiss, bewegt);
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "K" + super.toString();
+	}
+	@Override
+	public boolean spielZug(SpielFeld sp, Position von, Position nach) {
+		// TODO Auto-generated method stub
+		return super.spielZug(sp, von, nach);
+	}
+	@Override
+	public boolean spielzugMoeglich(SpielFeld sp, Position von, Position nach) {
+		
+		int absX = Math.abs(von.getX() - nach.getX());
+		int absY = Math.abs(von.getY() - nach.getY());
+		
+		if(!bewegt) {
+			return super.spielZug(sp, von, nach);
+		}else {
+			return super.spielZug(sp, von, nach) && absX <= 1 && absY <=1;
+		}
+	}
+}
