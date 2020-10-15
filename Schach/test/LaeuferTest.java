f0c0f4945aca9f096245cbe41fd9cb4aa740b1d3
myhpn <marcel.maffey@gmail.com>
2020-10-15 17:01:51 +0200

    Läufer: passt

---
+++ Schach/test/LaeuferTest.java
@@ -0,0 +1,31 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import game.Laeufer;
+import game.Position;
+import game.SpielFeld;
+import game.SpielFeldIO;
+import game.Springer;
+
+class LaeuferTest {
+	private static SpielFeld sp;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		SpielFeldIO spIO = new SpielFeldIO();
+		sp = spIO.einlesen("SpringerTest.txt");
+	}
+
+	@Test
+	public void testLäufer1() {
+		Laeufer l1 = (Laeufer) sp.getFeld(0, 5);
+		System.out.println(l1.toString());
+		boolean ok1 = l1.spielzugMoeglich(sp, new Position(0, 5), new Position(5, 0));
+		assertTrue(ok1);
+	}
+
+}
