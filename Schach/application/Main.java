c313c644540e62644f1089d79b2b6d20fcf0a8a6
micha <micha@DESKTOP-AQS87P5>
2020-10-23 22:04:34 +0200

    kurzer Test

--- Schach/application/Main.java
+++ Schach/application/Main.java
@@ -216,7 +216,7 @@ public class Main extends Application {
 		ausgabe.setPadding(new Insets(20));
 
 		SpielFeldIO spIO = new SpielFeldIO();
-		sp = spIO.einlesen("start.txt");
+		sp = spIO.einlesen("test4.txt");
 		sp.setWerAmZug(true);
 
 		for (int i = 1; i < 9; i++) {
